1fb9ac67398a4ab2205178b3a55e94b2bf4cbbac
Chris Karr <chris@audacious-software.com>
2012-02-27 09:59:28 -0800

    Minor tweaks

--- smsbot_twisted/local_settings.py
+++ smsbot_twisted/local_settings.py
@@ -2,7 +2,7 @@
 XMPP_JID = "smsbackend@boston.audacious-software.com/backend"
 XMPP_PASSWORD = "backend"
 XMPP_SERVER = "boston.audacious-software.com"
-XMPP_ENDPOINT = "smsbot@boston.audacious-software.com/bot"
+XMPP_ENDPOINT = "smsbot2@boston.audacious-software.com/smsbot"
 
 # HTTP API details 
 API_PORT = 58081
