3971b09c59ececa2f1449fb71092342665c64a62
Chris J. Karr <github@audacious-software.com>
2013-02-15 20:27:27 +0000

    :Adding local settings template

---
+++ smsbot_twilio/local_settings.py
@@ -0,0 +1,13 @@
+# XMPP server details
+XMPP_JID = "smsbot@localhost/twilio"
+XMPP_PASSWORD = "smsbot"
+XMPP_SERVER = "localhost"
+XMPP_ENDPOINT = "smsbot@localhost/backend"
+
+# HTTP API details 
+API_PORT = 18080
+
+# Twilio Details
+TWILIO_ACCOUNT = SECRET
+TWILIO_TOKEN = SECRET
+TWILIO_PHONE = SECRET 
