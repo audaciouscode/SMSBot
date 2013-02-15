e77e992d379f8c57559e7fba1c440452019c6a17
Chris J. Karr <github@audacious-software.com>
2013-02-15 20:28:52 +0000

    Adding local settings template

---
+++ smsbot_twisted/local_settings.py
@@ -0,0 +1,12 @@
+# XMPP server details
+XMPP_JID = "smsbot@localhost/backend"
+XMPP_PASSWORD = "SECRET"
+XMPP_SERVER = "localhost"
+XMPP_ENDPOINT = "smsbot@localhost/twilio"
+
+# HTTP API details 
+API_PORT = 58081
+
+# Django (or other service client) details
+CALLBACK_SECRET = 'SECRET'
+CALLBACK_URL = 'http://localhost/services/callback'
