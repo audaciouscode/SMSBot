74fc2421edcee0dac7702284027304b77173c8e9
Chris J. Karr <github@audacious-software.com>
2013-03-22 20:32:52 +0000

    Profile work

--- smsbot_web/smsbot/profiles/models.py
+++ smsbot_web/smsbot/profiles/models.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 from smsbot import local_settings
 
@@ -12,6 +13,20 @@ else:
 from django.conf.global_settings import LANGUAGES
 from django.contrib.auth.models import User
 from django.contrib.localflavor.us.models import PhoneNumberField
+from django.utils.encoding import smart_unicode
+
+def format_phone(phone_number):
+    phone_number = re.sub('[^0-9]', '', phone_number)
+
+    phone_digits_re = re.compile(r'^(?:1-?)?(\d{3})[-\.]?(\d{3})[-\.]?(\d{4})$')
+        
+    value = re.sub('(\(|\)|\s+)', '', smart_unicode(phone_number))
+    m = phone_digits_re.search(phone_number)
+
+    if m:
+        return u'%s-%s-%s' % (m.group(1), m.group(2), m.group(3))
+    
+    return None
 
 class UserProfile(models.Model):
     # User: username, name, e-mail
@@ -31,12 +46,73 @@ class UserProfile(models.Model):
         return self.user.username
         
     def best_phone(self):
-        try:
+        if self.phone_numbers.filter(active=True).order_by('priority').count() > 0:
             return self.phone_numbers.filter(active=True).order_by('priority')[0].value
-        except:
-            pass
+        else:
+            phone_number = re.sub('[^0-9]', '', self.user.username)
             
+            if len(phone_number) == 10:
+                self.set_primary_number(phone_number)
+                
+                return self.best_phone()
+           
         return None
+
+    def best_phones(self):
+        if self.best_phone():
+            return self.phone_numbers.filter(active=True).order_by('priority')
+        
+        return []
+        
+    def set_primary_number(self, phone_number):
+        phone_number = format_phone(phone_number)
+        
+        found = False
+
+        for phone in self.phone_numbers.all():
+            if phone.value == phone_number:
+                phone.priority = 0
+                phone.active = True
+                
+                found = True
+            else:
+                phone.priority = 10
+                
+            phone.save()
+            
+        if found == False:
+            phone = PhoneNumber(profile=self, value=phone_number)
+            phone.save()
+
+    def set_backup_number(self, phone_number):
+        if phone_number == None or phone_number.strip() == '':
+            for phone in self.phone_numbers.all():
+                if phone.priority != 0:
+                    phone.priority = 100
+                    phone.active = False
+                    phone.save()
+                                
+            return
+        
+        phone_number = format_phone(phone_number)
+
+        found = False
+
+        for phone in self.phone_numbers.all():
+            if phone.value == phone_number:
+                phone.priority = 10
+                phone.active = True
+
+                found = True
+            elif phone.priority != 0:
+                phone.priority = 100
+                
+            phone.save()
+            
+        if found == False:
+            phone = PhoneNumber(profile=self, value=phone_number, priority=10)
+            phone.save()
+            
     
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
@@ -49,3 +125,4 @@ class PhoneNumber(models.Model):
     active = models.BooleanField(default=True)
     
     best_time = models.TimeField(default=datetime.time(12, 0, 0))
+
--- smsbot_web/smsbot/smsbot/settings.py
+++ smsbot_web/smsbot/smsbot/settings.py
@@ -146,6 +146,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
 )
 
+AUTH_PROFILE_MODULE = 'profiles.UserProfile'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
