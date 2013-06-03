b8b73ca0745369272b7d832d299da08befe8fd7a
Chris J. Karr <github@audacious-software.com>
2013-06-03 13:05:51 +0000

    Adding statistical methods

--- smsbot_django/profiles/models.py
+++ smsbot_django/profiles/models.py
@@ -118,6 +118,71 @@ class UserProfile(models.Model):
 
     def tagged_message_count(self, variable, start, end):
         return self.scripts.filter(start_date__gte=start, start_date__lt=end, tags__icontains=variable).count()
+        
+    def msg_statistics(self):
+        from events.models import MessageEvent, ScriptEvent
+
+        stats = []
+
+        now = datetime.datetime.now()
+        
+        ranges = []
+        ranges.append(('Today', now, datetime.timedelta(1)))
+        ranges.append(('Yesterday', now - datetime.timedelta(1), datetime.timedelta(1)))
+        ranges.append(('This Week', now, datetime.timedelta(7)))
+        ranges.append(('Last Week', now - datetime.timedelta(7), datetime.timedelta(7)))
+        ranges.append(('Month', now, datetime.timedelta(30)))
+        ranges.append(('Year', now, datetime.timedelta(365)))
+        ranges.append(('All', now, datetime.timedelta(365 * 100)))
+
+        
+        for range in ranges:
+            date_stats = {}
+            date_stats['period'] = range[0]
+            
+            phone = self.best_phone().replace('-', '')
+            
+            start = range[1] - range[2]
+            
+            date_stats['sent'] = MessageEvent.objects.filter(sender__contains=phone, date__lte=range[1], date__gte=start).count()
+            date_stats['received'] = MessageEvent.objects.filter(recipient__contains=phone, date__lte=range[1], date__gte=start).count()
+            date_stats['responses'] = ScriptEvent.objects.filter(event='script_session_variable_set', date__lte=range[1], date__gte=start, script__recipient=self).count()
+            
+            if date_stats['received'] > 0:
+                date_stats['response_rate'] = float(date_stats['responses']) / float(date_stats['received']) * 100
+            else:
+                date_stats['response_rate'] = 'N/A'
+                
+            latency_elapsed = 0.0
+            session_count = 0
+            
+            sessions = []
+            
+            for event in ScriptEvent.objects.filter(date__lte=range[1], date__gte=start, script__recipient=self).order_by('date'):
+                if (event.script in sessions) == False:
+                    sessions.append(event.script)
+                    
+            for script in sessions:
+                start = ScriptEvent.objects.filter(script=script, event='script_session_started')[0]
+
+                responses = ScriptEvent.objects.filter(script=script, event='script_session_variable_set')
+                
+                if responses.count() > 0:
+                    end = responses[0]
+                    
+                    diff = end.date - start.date
+                    
+                    session_count += 1
+                    latency_elapsed += diff.seconds + (24 * 60 * 60 * diff.days)
+
+            if session_count > 0:
+                date_stats['latency'] = (latency_elapsed / session_count) / 60
+            else:
+                date_stats['latency'] = 'N/A'
+            
+            stats.append(date_stats)
+        
+        return stats
             
     
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
